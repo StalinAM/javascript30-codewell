eb5d694a814e1f74e4829f5213b2e3d53a43e2cf
Stalin Acurio <sta_vla411@hotmail.com>
2023-08-14 23:11:34 -0500

    add logic for open and close menu

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("hello world");
+const openMenu = document.getElementById("icon-burger");
+const menuModal = document.getElementById("menu-modal");
+const closeMenu = document.getElementById("icon-close");
+
+closeMenu.addEventListener("click", () => (menuModal.style.display = "none"));
+openMenu.addEventListener("click", () => (menuModal.style.display = "flex"));
