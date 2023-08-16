d996030ca032b71ef32048b6b961aff8af389999
Stalin Acurio <sta_vla411@hotmail.com>
2023-08-15 22:01:48 -0500

    add logic for open and close menu in product item

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const openMenu = document.getElementById("icon-burger");
 const menuModal = document.getElementById("menu-modal");
 const closeMenu = document.getElementById("icon-close");
+const openMenuP = document.getElementById("product");
 
 closeMenu.addEventListener("click", () => (menuModal.style.display = "none"));
 openMenu.addEventListener("click", () => (menuModal.style.display = "flex"));
+openMenuP.addEventListener("click", () => {
+  menuModal.style.display !== "none"
+    ? (menuModal.style.display = "flex")
+    : (menuModal.style.display = "none");
+  menuModal.style.display === "flex"
+    ? (menuModal.style.display = "none")
+    : (menuModal.style.display = "flex");
+});
