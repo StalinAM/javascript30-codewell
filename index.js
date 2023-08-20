5e3bcdf0cd9c4fc243c1f0efa854a304ef903888
Stalin <sta_vla411@hotmail.com>
2023-08-20 01:43:47 -0500

    change position of menu modal

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ const openMenuP = document.getElementById("product");
 closeMenu.addEventListener("click", () => (menuModal.style.display = "none"));
 openMenu.addEventListener("click", () => (menuModal.style.display = "flex"));
 openMenuP.addEventListener("click", () => {
-  menuModal.style.display !== "none"
-    ? (menuModal.style.display = "flex")
-    : (menuModal.style.display = "none");
   menuModal.style.display === "flex"
     ? (menuModal.style.display = "none")
     : (menuModal.style.display = "flex");
+  menuModal.style.display !== "none"
+    ? (menuModal.style.display = "flex")
+    : (menuModal.style.display = "none");
 });
+if (screen.width > 780) {
+  const tops = openMenuP.getBoundingClientRect().top;
+  const left = openMenuP.getBoundingClientRect().left;
+  menuModal.style.left = `${left - 50}px`;
+  menuModal.style.top = `${tops + 35}px`;
+}
